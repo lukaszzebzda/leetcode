8c159fe322fb798174e5ba863a64070b253704ec
Łukasz Zebzda <zeblukasz@gmail.com>
2025-06-29 17:09:39 +0200

    Solution to question 28 "28. Find the Index of the First Occurrence in a String" Notes: Quick answer is just haystack.indexOf(needle), however you can try doing it by yourself, but in this case you lose time to program that, you welcome bugs, lose on readability. Only if given use situation dictate otherwise, I would rather stick to the well known indexOf function.

---
+++ src/Solutions/firstOccurIndex28.java
@@ -0,0 +1,12 @@
+package Solutions;
+
+public class firstOccurIndex28 {
+
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
+    }
+
+    public static int strStr(String haystack, String needle) {;
+        return haystack.indexOf(needle);
+    }
+}
