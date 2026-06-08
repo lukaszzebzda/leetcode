4f72c99c57ca9de09d413720d2ee8aaef1ccd14d
Łukasz Zebzda <zeblukasz@gmail.com>
2026-06-08 22:26:27 +0200

    Solution to question 560 "Subarray Sum Equals K" Notes: First approach that I can think of is looping through every possible subarray combination to finally check how many of their sums was equal to given K.

---
+++ src/Solutions/SubarraySumEqualsK560.java
@@ -0,0 +1,14 @@
+package Solutions;
+
+public class SubarraySumEqualsK560 {
+
+    public static void main(String[] args) {
+        int[] arr = {10, 2, -2, -20, 10};
+        int k = -10;
+        System.out.println(subarraySum(arr, k));
+    }
+
+    public static int subarraySum(int[] nums, int k) {
+
+    }
+}
