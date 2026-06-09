196c2ed3b998189714fb5c6639af9b1dc700088e
Łukasz Zebzda <zeblukasz@gmail.com>
2026-06-10 00:16:47 +0200

    Solution to question 141 "LindeListCycle" Notes: In first aproach which is naive, you just run through nodes, packing every spoted one int some hashset, but when you stub upon one that is already in said hashSet, you have a loop, otherwise you dont.  The faster aproach is using pattern "two pointers" in which we run two pointers, one by one step at a loop and second by two steps. If faster pointer finds empty tail, it return false, but if at some iteration fastere pointer catch up to the slower it means there was a loop.

---
+++ src/Solutions/LinkedListCycle141.java
@@ -0,0 +1,69 @@
+package Solutions;
+
+
+import java.util.HashSet;
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class LinkedListCycle141 {
+
+    public static void main(String[] args) {
+        System.out.println("Solution");
+    }
+
+    public static boolean hasCycle(ListNode head) {
+        HashSet<ListNode> aknwoledged = new HashSet<>();
+
+
+        while(head != null){
+            if(aknwoledged.contains(head)){
+                return true;
+            }
+
+            aknwoledged.add(head);
+
+            head = head.next;
+        }
+
+        return false;
+    }
+
+    public static boolean hasCycle2(ListNode head) {
+        ListNode slowPointer = head, fastPointer = head;
+        if(head == null || fastPointer.next == null){
+            return false;
+        }
+
+        while(fastPointer != null && fastPointer.next != null){
+            slowPointer = slowPointer.next;
+            fastPointer = fastPointer.next.next;
+
+
+            if(slowPointer == fastPointer){
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
